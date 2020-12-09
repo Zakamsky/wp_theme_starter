201d8043b1a6880e6d5e8502b1fdb2b5cc52f3cd
Aleksei Elkin <alex.zakamsky@gmail.com>
2020-12-09 15:24:49 +0100

    Update postcss.config.js

--- resources/postcss.config.js
+++ resources/postcss.config.js
@@ -2,7 +2,9 @@ module.exports = ({ options }) => ({
   plugins: {
     'autoprefixer': {},
     'postcss-preset-env': {},
-    'css-mqpacker': {},
+    'css-mqpacker': {
+      sort: true
+    },
     'cssnano': options.dev ? false : {
       preset: ['default', {
         discardComments: { removeAll: true }
